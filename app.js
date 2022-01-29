118363ca4329795078d4d2fa8028c54f1b2e5bcc
Rochelle Younger <regnuoy321@gmail.com>
2022-01-29 15:44:27 -0500

    created HTML template and modularized code

--- app.js
+++ app.js
@@ -3,18 +3,32 @@
 
 // console.log(process);
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+//     console.log("=========================");
 
-    console.log("=========================");
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+// // const printProfileData = profileDataArr => console.log(profileDataArr);
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-// const printProfileData = profileDataArr => console.log(profileDataArr);
+// printProfileData(profileDataArgs);
 
-printProfileData(profileDataArgs);
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
+
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+const profileDataArgs = process.argv.slice(2);
+
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+//same as ...
+const [name, github] = profileDataArgs;
+
+fs.writeFile("index.html", generatePage(name, github), err => {
+    if (err) throw new Error (err);
+
+    console.log("Portfolio complete! Check out index.html to see the output!");
+});
