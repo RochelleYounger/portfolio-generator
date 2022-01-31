ae5e9ae81ba7e8c9d89146c0e2803ea34446fc79
Rochelle Younger <regnuoy321@gmail.com>
2022-01-31 14:58:35 -0500

    styled site

--- app.js
+++ app.js
@@ -18,7 +18,9 @@
 
 /********************************************************************************/
 const inquirer = require("inquirer");
-const fs = require("fs");
+// const fs = require("fs");
+// const generateSite = require("./utils/generate-site.js");
+const { writeFile, copyFile } = require("./utils/generate-site.js");
 const generatePage = require("./src/page-template.js");
 
 // const pageHTML = generatePage(name, github);
@@ -156,15 +158,44 @@ const promptProject = portfolioData => {
     });
 };
 
+// promptUser()
+//     .then(promptProject)
+//     .then(portfolioData => {
+//         const pageHTML = generatePage(portfolioData);
+        
+//         fs.writeFile("./dist/index.html", pageHTML, err => {
+//             if (err) throw new Error(err);
+
+//             console.log("Page created! Check out index.html in this directory to see it!");
+            
+//             fs.copyFile('./src/style.css', './dist/style.css', err => {
+//                 if (err) throw new Error(err);
+//                 // if (err) {
+//                 //     console.log(err);
+//                 //     return;
+//                 // }
+//                 console.log('Style sheet copied successfully!');
+//             });
+//         });
+//     })
+// ;
+
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData);
-        
-        fs.writeFile("./index.html", pageHTML, err => {
-            if (err) throw new Error(err);
-
-            console.log("Portfolio complete! Check out index.html to see the output!");
-        });
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
     })
 ;
